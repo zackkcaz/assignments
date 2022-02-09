13b9f0b522b93852394e99bfc89e7873440de43d
Raggedy Shaggedy <94563960+zackkcaz@users.noreply.github.com>
2022-02-09 14:40:43 -0500

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,9 @@
+#ZackAssignment1.py
+#zackD
+#feb 9 2022
+for i in range (100):
+ num1 = input ('enter a number ')
+ num2 = input ('enter a 2nd number ')
+ a = int(num1)
+ b = int(num2)
+ print (a + b)
